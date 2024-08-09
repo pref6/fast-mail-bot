df170a912ee4a30171293752afc0a43c03203f92
pref6 <160345597+pref6@users.noreply.github.com>
2024-08-09 17:24:13 +0400

    Add files via upload

---
+++ Config.py
@@ -0,0 +1,11 @@
+import os
+
+BOT_TOKEN = "BOT_TOKEN"
+API_ID = "API_ID"
+API_HASH = "API_HASH"
+
+# "API_ID" - Get this value from https://my.telegram.org
+# "API_HASH" - Get this value from https://my.telegram.org
+# "BOT_TOKEN" - Get Bot Token From BotFather Bot
+
+# The bot was created and run on a version of python-3.10.7
